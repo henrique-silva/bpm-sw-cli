d482d13b7630b503575c75bc3326d785f85589e6
Henrique Silva <henrique.silva@lnls.br>
2015-02-24 11:28:09 -0300

    include script to perform a power sweep test with an RF generator
    Interfaces with the RS_SMB100A through the RS_gen class

---
+++ scripts/run_pow_sweep.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+def run_pow_sweep(argv):
+    import sys
+    import os
+    import argparse
+    from time import strftime
+    from run_single import run_single
+    from bpm_experiment import BPMExperiment
+    from RS_SMB100A import RS_gen
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('metadata', help='metadata file path')
+    parser.add_argument('output', help='folder where the output data will be saved')
+    parser.add_argument('start', help='sweep initial power', type=int,default=-60)
+    parser.add_argument('stop', help='sweep final power', type=int, default=0)
+    parser.add_argument('step', help='sweep power step', type=int, default=10)
+    parser.add_argument('-g','--genip', help='generator ip address', default='10.0.17.44')
+    parser.add_argument('-e','--endpoint', help='broker endpoint', default='tcp://10.0.18.39:8888')
+    parser.add_argument('-d','--board', type=int, choices=[0,1,2,3,4,5], help='select the target board for the test', default='0')
+    parser.add_argument('-b','--bpm', type=int, choices=[0,1], help='select the target bpm for the test', default='0')
+    parser.add_argument('-r','--rffeconfig', action='store_true', help='enable the rffe configuration process', default=False)
+    parser.add_argument('-a','--allboards', action='store_true', help='run the script for all boards and bpms', default=False)
+    parser.add_argument('-l','--loss', type=float, help='external power loss in the test', default=6.8)
+    parser.add_argument('-f','--frequency', type=float, help='set generator frequency', default=477999596)
+    args = parser.parse_args(argv)
+
+    exp = BPMExperiment(args.endpoint)
+
+    #Configure the RF Generator
+    gen = RS_gen(args.genip)
+    gen.set_freq(args.frequency)
+
+    for Pout in range(args.start,args.stop,args.step):
+        gen.rf_on()
+        Pout = Pout + args.loss
+        print('\n\n\n======================================================')
+        print('New test initiated at ' + strftime('%Y-%m-%d %H:%M:%S'))
+        print('======================================================')
+
+        gen.set_pow(Pout)    
+        print('Current Power (RFFE input) = '+str(Pout-args.loss)+' dBm')
+
+        #Use a temporary metadata to pass the input power to the called functions
+        exp.load_from_metadata(args.metadata)
+        exp.metadata['rffe_signal_carrier_inputpower'] = str(Pout-args.loss) + ' dBm'
+        with open(args.metadata+'.temp','w') as temp:
+            temp.writelines(''.join(sorted(exp.get_metadata_lines())))
+            temp_path = os.path.abspath(temp.name)
+
+        single_args = [temp_path, args.output, '-e' ,args.endpoint, '-p', 'fofb', '-s']
+        if args.allboards:
+            single_args.extend(['-a'])
+        else:
+            single_args.extend(['-d', str(args.board),'-b', str(args.bpm)])
+        if args.rffeconfig:
+            single_args.extend(['-r'])
+        run_single(single_args)
+        os.remove(temp_path)
+        
+    gen.set_pow(-80)
+    gen.rf_off()
+    
+if __name__ == "__main__":
+    import sys
+    run_pow_sweep(sys.argv[1:])
