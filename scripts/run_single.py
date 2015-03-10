86d30445ed8b61b9d89de7e9d600e5ec554181dc
Henrique Silva <henrique.silva@lnls.br>
2015-03-10 10:29:42 -0300

    scripts/run_*.py: update python interpreter folder
    The python3 was installed in a different folder on the DIG Virtual
    Machine

--- scripts/run_single.py
+++ scripts/run_single.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 def run_single(argv):
     import sys
--- scripts/run_sweep.py
+++ scripts/run_sweep.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 
 def run_sweep(argv):
     import sys
