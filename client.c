41524a20832687a167a4fbb1613e5e2718d79bac
Henrique Silva <henrique.silva@lnls.br>
2015-03-12 14:30:40 -0300

    Revert "client.c: avoid strict alias breaking for double arguments"
    This reverts commit 1d4091b80a61842d3f7312d8d95c1881c99385c5.

--- client.c
+++ client.c
@@ -121,12 +121,13 @@ int print_var (call_var_t *var)
             printf ("%" PRIu64 "\n", *(read_val_ptr64));
             break;
 
-        case DISP_ATYPE_DOUBLE:
-            printf ("%f\n", (double)*(var->read_val));
+        case DISP_ATYPE_DOUBLE:;
+            double* read_val_ptr_dbl = (double *)var->read_val;
+            printf ("%f\n", *(read_val_ptr_dbl));
             break;
 
         default:
-            printf ("%" PRIu8 "\n", ((uint8_t )*(var->read_val)));
+            printf ("%" PRIu16 "\n", ((uint16_t )*(var->read_val)));
     }
     return 0;
 }
