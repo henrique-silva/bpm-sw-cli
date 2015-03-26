6ec6415546e4582cd1fdedc77ee0b379b543da3d
Henrique Silva <henrique.silva@lnls.br>
2015-03-26 16:20:49 -0300

    scripts/acq/run_single.py: include switching sweep

--- scripts/acq/run_single.py
+++ scripts/acq/run_single.py
@@ -14,11 +14,15 @@ def run_single(argv):
     parser.add_argument('-e','--endpoint', help='broker endpoint', default='tcp://10.2.117.47:8888')
     parser.add_argument('-d','--board', type=int, help='select the target board for the test', action='append')
     parser.add_argument('-b','--bpm', type=int, choices=[0,1], help='select the target bpm for the test', action='append')
+    parser.add_argument('-f','--afc', help='AFC Board name', default='AFC1')
+    parser.add_argument('-m','--fmc', help='FMC Board name', default='V3P1')
     parser.add_argument('-s','--silent', action='store_true', help='run the script without asking for confirmation', default=False)
     parser.add_argument('-r','--rffeconfig', action='store_true', help='enable the rffe configuration process', default=False)
     parser.add_argument('-a','--allboards', action='store_true', help='run the script for all boards and bpms', default=False)
     parser.add_argument('-t','--temperature', action='store_true', help='enable rack temperature reading', default=False)
     parser.add_argument('-c','--fmcconfig', action='store_true', help='perform only the acquisition, not configuring the FMC board', default=False)
+    parser.add_argument('-w','--swsweep', action='store_true', help='perform acquistion sweeping the switching', default=False)
+    
     args = parser.parse_args(argv)
 
     exp = bpm_experiment.BPMExperiment(args.endpoint)
@@ -34,6 +38,11 @@ def run_single(argv):
     else:
         board = args.board
         bpm = args.bpm
+
+    if args.swsweep:
+        sw_sweep = ['on','off']
+    else:
+        sw_sweep = ['off']
 
     while True:
         exp.load_from_metadata(args.metadata)
@@ -63,40 +72,36 @@ def run_single(argv):
                     # Assure that no file or folder will be overwritten
                     ntries = 1;
                     date = strftime('%d-%m-%Y')
-                    board_path = 'board'+str(board_number)
-                    bpm_path = 'bpm'+str(bpm_number)
 
-                    #Replace the dot and space in power level string with an underline to avoid problems when creating the folder
-                    power_level = exp.metadata['rffe_signal_carrier_inputpower'].replace(".","_")
-                    power_level = power_level.replace(" ","_")
-
-                    print('\n        Using Board '+str(board_number)+ ' and BPM '+str(bpm_number)+'...')
-                    while True:
-                        data_filenames = []
-                        for datapath in args.datapath:
-                            if exp.metadata['signal_source'] == 'signalgenerator':
-                                data_filenames.append(os.path.join(os.path.normpath(args.output), date, board_path, bpm_path, power_level, datapath, 'data_' + str(ntries) + '_' + datapath + '.txt'))
+                    for sw in sw_sweep:
+                        print('\n        Using Board '+str(board_number)+ ' and BPM '+str(bpm_number)+' with Switching '+sw+' ...')
+                        exp.metadata['rffe_switching'] = sw
+                        while True:
+                            data_filenames = []
+                            for datapath in args.datapath:
+                                if 'fofb' or 'tbt' in datapath:
+                                    data_filenames.append(os.path.join(os.path.normpath(args.output), date, args.afc+'_'+args.fmc, datapath, 'sw_'+sw, 'data_' + str(ntries) + '_' + datapath, 'data_' + str(ntries) + '_' + datapath + '.txt'))
+                                else:
+                                    data_filenames.append(os.path.join(os.path.normpath(args.output), date, args.afc+'_'+args.fmc, datapath, 'sw_'+sw, 'data_' + str(ntries) + '_' + datapath + '.txt'))
+                            ntries = ntries+1
+                            if all(not os.path.exists(data_filename) for data_filename in data_filenames):
+                                break
+                        for i in range(0,len(data_filenames)):
+                            print('        Running ' + args.datapath[i] + ' datapath...')
+                            sys.stdout.flush()
+                            try:
+                                exp.run(data_filenames[i], args.datapath[i], str(board_number), str(bpm_number), args.fmcconfig, args.rffeconfig)
+                            except bpm_experiment.OverPowerError as e:
+                                print ('The power level '+str(e.value)+' will damage the RFFE so it\'ll be skipped!')
+                                continue
+                            except bpm_experiment.BoardTimeout:
+                                print ('This Board doesn\'t respond and will be skipped!')
+                                break
+                            except bpm_experiment.RFFETimeout:
+                                print ('RFFE board doesn\'t respond!')
+                                b
                             else:
-                                data_filenames.append(os.path.join(os.path.normpath(args.output), date, board_path, bpm_path, datapath, 'data_' + str(ntries) + '_' + datapath + '.txt'))
-                        ntries = ntries+1
-                        if all(not os.path.exists(data_filename) for data_filename in data_filenames):
-                            break
-                    for i in range(0,len(data_filenames)):
-                        print('        Running ' + args.datapath[i] + ' datapath...')
-                        sys.stdout.flush()
-                        try:
-                            exp.run(data_filenames[i], args.datapath[i], str(board_number), str(bpm_number), args.fmcconfig, args.rffeconfig)
-                        except bpm_experiment.OverPowerError as e:
-                            print ('The power level '+str(e.value)+' will damage the RFFE so it\'ll be skipped!')
-                            continue
-                        except bpm_experiment.BoardTimeout:
-                            print ('This Board doesn\'t respond and will be skipped!')
-                            continue
-                        except bpm_experiment.RFFETimeout:
-                            print ('RFFE board doesn\'t respond!')
-                            continue
-                        else:
-                            print(' done. Results in: ' + data_filenames[i])
+                                print(' done. Results in: ' + data_filenames[i])
             break
 
         elif input_text == 'q':
